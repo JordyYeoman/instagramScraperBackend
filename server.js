8d04ba291775fcf68067d0c5b4911388750f33b6
JordyYeoman <jordyyeoman@gmail.com>
2020-09-12 15:58:02 +0800

    Restrict Certain Routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 const mongoose = require("mongoose");
 const morgan = require('morgan');
+const helmet = require('helmet');
 const bodyParser = require("body-parser");
 const ImageData = require("./models/ImageDataModel");
 const cors = require("cors");
@@ -12,6 +15,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 // Middlwares
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(morgan(':method :url :status'));
 app.use(cors());
@@ -52,10 +56,42 @@ app.get("/data", async (req, res) => {
     // Leaving the Post.find() method empty
     // means we want to return the entire collection of data
     try {
-        const posts = await ImageData.find();
-        res.json(posts);
+        const images = await ImageData.find();
+        res.json(images);
     } catch (err) {
         res.json({ message: err });
     }
 });
 
+const jwtCheck = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-8ff1npvg.au.auth0.com/.well-known/jwks.json'
+    }),
+
+    // Validate the audience and the issuer.
+    audience: 'https://instagramScrapAPI/',
+    issuer: 'https://dev-8ff1npvg.au.auth0.com/',
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
+// POST routes
+app.post("/postdata", async (req, res) => {
+    console.log("I got a request");
+    // Array of data that needs to be inserted to MongoDB
+    const data = req.body;
+
+    ImageData.insertMany(data)
+        .then((result) => {
+            console.log("result ", result);
+            res.status(200).json({ success: "new documents added!", data: result });
+        })
+        .catch((err) => {
+            console.error("error ", err);
+            res.status(400).json({ err });
+        });
+});
